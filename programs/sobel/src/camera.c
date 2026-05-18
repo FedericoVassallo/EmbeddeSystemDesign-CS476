41d2ea59d95dc6a9ae3b263da7540da2714fe836
FedericoVassallo <federico.vassallo2003@gmail.com>
2026-05-18 16:13:51 +0200

    updated camera.c in sobel folder to only do sobel

--- programs/sobel/src/camera.c
+++ programs/sobel/src/camera.c
@@ -12,6 +12,7 @@ volatile uint16_t rgb565[640*480];
 volatile uint8_t grayscale[640*480];
 // volatile uint8_t floyd[640*480];
 // volatile int16_t error_array[642<<1];
+volatile uint8_t sobelResult[640*480]; // added output buffer for Sobel
 
 int main () {
   volatile int result;
@@ -36,67 +37,20 @@ int main () {
   vga[1] = swap_u32(result);
   printf("PCLK (kHz) : %d\n", camParams.pixelClockInkHz );
   printf("FPS        : %d\n", camParams.framesPerSecond );
-  while(1) {
-    vga[2] = swap_u32(1);
-    vga[3] = swap_u32((uint32_t) &rgb565[0]);
-    asm volatile ("l.nios_rrr r0,%[in1],%[in2],0x6"::[in1]"r"(5000000),[in2]"r"(1)); // set 5 seconds
-    do {
-      takeSingleImageBlocking((uint32_t) &rgb565[0]);
-      asm volatile ("l.nios_rrr %[out1],r0,%[in2],0x6":[out1]"=r"(result):[in2]"r"(3));
-    } while (result != 0);
-    vga[2] = swap_u32(2);
-    vga[3] = swap_u32((uint32_t) &grayscale[0]);
-    asm volatile ("l.nios_rrr r0,%[in1],%[in2],0x6"::[in1]"r"(5000000),[in2]"r"(1)); // set 5 seconds
-    uint32_t * rgb = (uint32_t *) &rgb565[0];
-    uint32_t * gray = (uint32_t *) &grayscale[0];
-    uint32_t grayPixels;
-    do {
-      takeSingleImageBlocking((uint32_t) &rgb565[0]);
-      for (int line = 0; line < camParams.nrOfLinesPerImage; line++) {
-        for (int pixel = 0; pixel < camParams.nrOfPixelsPerLine; pixel++) {
-          uint16_t rgb = swap_u16(rgb565[line*camParams.nrOfPixelsPerLine+pixel]);
-          uint32_t red1 = ((rgb >> 11) & 0x1F) << 3;
-          uint32_t green1 = ((rgb >> 5) & 0x3F) << 2;
-          uint32_t blue1 = (rgb & 0x1F) << 3;
-          uint32_t gray = ((red1*54+green1*183+blue1*19) >> 8)&0xFF;
-          grayscale[line*camParams.nrOfPixelsPerLine+pixel] = gray;
-        }
+    while(1) {
+    vga[2] = swap_u32(2); // 2 to set grayscale
+    vga[3] = swap_u32((uint32_t) &sobelResult[0]); // tell HDMI which buffer to display
+    takeSingleImageBlocking((uint32_t) &rgb565[0]);
+    for (int line = 0; line < camParams.nrOfLinesPerImage; line++) {
+      for (int pixel = 0; pixel < camParams.nrOfPixelsPerLine; pixel++) {
+        uint16_t rgb = swap_u16(rgb565[line*camParams.nrOfPixelsPerLine+pixel]);
+        uint32_t red1 = ((rgb >> 11) & 0x1F) << 3;
+        uint32_t green1 = ((rgb >> 5) & 0x3F) << 2;
+        uint32_t blue1 = (rgb & 0x1F) << 3;
+        uint32_t gray = ((red1*54+green1*183+blue1*19) >> 8)&0xFF;
+        grayscale[line*camParams.nrOfPixelsPerLine+pixel] = gray;
       }
-      asm volatile ("l.nios_rrr %[out1],r0,%[in2],0x6":[out1]"=r"(result):[in2]"r"(3));
-    } while (result != 0);
-    vga[2] = swap_u32(2);
-    vga[3] = swap_u32((uint32_t) &floyd[0]);
-    asm volatile ("l.nios_rrr r0,%[in1],%[in2],0x6"::[in1]"r"(5000000),[in2]"r"(1)); // set 5 seconds
-    do {
-      takeSingleImageBlocking((uint32_t) &rgb565[0]);
-      for (int line = 0; line < camParams.nrOfLinesPerImage; line++) {
-        for (int pixel = 0; pixel < camParams.nrOfPixelsPerLine; pixel++) {
-          uint16_t rgb = swap_u16(rgb565[line*camParams.nrOfPixelsPerLine+pixel]);
-          uint32_t red1 = ((rgb >> 11) & 0x1F) << 3;
-          uint32_t green1 = ((rgb >> 5) & 0x3F) << 2;
-          uint32_t blue1 = (rgb & 0x1F) << 3;
-          uint32_t gray = ((red1*54+green1*183+blue1*19) >> 8)&0xFF;
-          grayscale[line*camParams.nrOfPixelsPerLine+pixel] = gray;
-        }
-      }
-      floyd_steinberg(grayscale, camParams.nrOfPixelsPerLine, camParams.nrOfLinesPerImage, floyd, error_array);
-      asm volatile ("l.nios_rrr %[out1],r0,%[in2],0x6":[out1]"=r"(result):[in2]"r"(3));
-    } while (result != 0);
-    asm volatile ("l.nios_rrr r0,%[in1],%[in2],0x6"::[in1]"r"(5000000),[in2]"r"(1)); // set 5 seconds
-    do {
-      takeSingleImageBlocking((uint32_t) &rgb565[0]);
-      for (int line = 0; line < camParams.nrOfLinesPerImage; line++) {
-        for (int pixel = 0; pixel < camParams.nrOfPixelsPerLine; pixel++) {
-          uint16_t rgb = swap_u16(rgb565[line*camParams.nrOfPixelsPerLine+pixel]);
-          uint32_t red1 = ((rgb >> 11) & 0x1F) << 3;
-          uint32_t green1 = ((rgb >> 5) & 0x3F) << 2;
-          uint32_t blue1 = (rgb & 0x1F) << 3;
-          uint32_t gray = ((red1*54+green1*183+blue1*19) >> 8)&0xFF;
-          grayscale[line*camParams.nrOfPixelsPerLine+pixel] = gray;
-        }
-      }
-      edgeDetection(grayscale,floyd, camParams.nrOfPixelsPerLine, camParams.nrOfLinesPerImage,128);
-      asm volatile ("l.nios_rrr %[out1],r0,%[in2],0x6":[out1]"=r"(result):[in2]"r"(3));
-    } while (result != 0);
+    }
+    edgeDetection(grayscale, sobelResult, camParams.nrOfPixelsPerLine, camParams.nrOfLinesPerImage, 128); // output buffer is sobelResult
   }
 }
