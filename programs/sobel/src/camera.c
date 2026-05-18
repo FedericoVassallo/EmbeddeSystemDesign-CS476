4cde550181a3b04b7a70a32a9c673685b3dd17ac
FedericoVassallo <federico.vassallo2003@gmail.com>
2026-05-18 18:13:39 +0200

    push

--- programs/sobel/src/camera.c
+++ programs/sobel/src/camera.c
@@ -20,6 +20,7 @@ int main () {
   volatile int result;
   volatile unsigned int *vga = (unsigned int *) 0X50000020;
   camParameters camParams;
+  volatile uint32_t cycles,stall,idle;
 
   volatile uint8_t *sobelCurr = sobelA;   // where this frame's edges go
   volatile uint8_t *sobelPrev = sobelB;   // last frame's edges
@@ -44,10 +45,12 @@ int main () {
   printf("PCLK (kHz) : %d\n", camParams.pixelClockInkHz );
   printf("FPS        : %d\n", camParams.framesPerSecond );
   for (int i = 0; i < 640*480; i++) { sobelA[i] = 0; sobelB[i] = 0; } // clear buffers
-    while(1) {
+
+  while(1) {
     vga[2] = swap_u32(2); // 2 to set grayscale
     vga[3] = swap_u32((uint32_t) &motion[0]); // tell HDMI which buffer to display
     takeSingleImageBlocking((uint32_t) &rgb565[0]);
+    asm volatile ("l.nios_rrr r0,r0,%[in2],0xB"::[in2]"r"(7)); // start the Profiling
     for (int line = 0; line < camParams.nrOfLinesPerImage; line++) {
       for (int pixel = 0; pixel < camParams.nrOfPixelsPerLine; pixel++) {
         uint16_t rgb = swap_u16(rgb565[line*camParams.nrOfPixelsPerLine+pixel]);
@@ -63,7 +66,7 @@ int main () {
     int totalPixels = camParams.nrOfPixelsPerLine * camParams.nrOfLinesPerImage;
     for (int i = 0; i < totalPixels; i++) {
       int diff = (int)sobelCurr[i] - (int)sobelPrev[i];
-      if (diff < 0) diff = -diff;
+      // if (diff < 0) diff = -diff; this is commented for new we have to decide if we want to show both edges or only the new one
       motion[i] = (diff > 0) ? 255 : 0;
     }
 
@@ -71,5 +74,11 @@ int main () {
     volatile uint8_t *temp = sobelPrev;
     sobelPrev = sobelCurr;
     sobelCurr = temp;
+
+    // stop the profiling and then print the cycles
+    asm volatile ("l.nios_rrr %[out1],r0,%[in2],0xB":[out1]"=r"(cycles):[in2]"r"(1<<8|7<<4));
+    asm volatile ("l.nios_rrr %[out1],%[in1],%[in2],0xB":[out1]"=r"(stall):[in1]"r"(1),[in2]"r"(1<<9));
+    asm volatile ("l.nios_rrr %[out1],%[in1],%[in2],0xB":[out1]"=r"(idle):[in1]"r"(2),[in2]"r"(1<<10));
+    printf("nrOfCycles: %d %d %d\n", cycles, stall, idle);
   }
 }
